a75aef04dd50b764b934e987759cc356288e2d7e
Code's Garden <72145188+Galaxy-Ranger@users.noreply.github.com>
2025-01-23 09:25:47 +0000

    creating some files

---
+++ 07_Javascript_in_the_browser/JS/Exercises/04_Clock.js
@@ -0,0 +1,3 @@
+// Create a digital second clock using setInterval and date object in javascript.
+// The date object can be used to get the date, time, hours, and seconds which can be updated using setInterval.
+// Try to keep the UI good looking.
